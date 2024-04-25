6c9b18ddd35ef97f0dee37680d013b2e9d16f35f
kkuokkan <kerkko.kuokkanen@gmail.com>
2024-04-25 13:21:45 +0300

    new sprites

---
+++ srcs/ability/damages/damageCreator.d
@@ -0,0 +1,500 @@
+srcs/ability/damages/damageCreator.o: \
+  srcs/ability/damages/damageCreator.cpp \
+  srcs/ability/damages/../../../hdr/global.h hdr/ab.h hdr/../SDL2/SDL.h \
+  hdr/../SDL2/SDL_main.h hdr/../SDL2/SDL_stdinc.h \
+  hdr/../SDL2/SDL_config.h hdr/../SDL2/SDL_platform.h \
+  hdr/../SDL2/begin_code.h hdr/../SDL2/close_code.h \
+  hdr/../SDL2/SDL_assert.h hdr/../SDL2/SDL_atomic.h \
+  hdr/../SDL2/SDL_audio.h hdr/../SDL2/SDL_error.h \
+  hdr/../SDL2/SDL_endian.h hdr/../SDL2/SDL_mutex.h \
+  hdr/../SDL2/SDL_thread.h hdr/../SDL2/SDL_rwops.h \
+  hdr/../SDL2/SDL_clipboard.h hdr/../SDL2/SDL_cpuinfo.h \
+  hdr/../SDL2/SDL_events.h hdr/../SDL2/SDL_video.h \
+  hdr/../SDL2/SDL_pixels.h hdr/../SDL2/SDL_rect.h \
+  hdr/../SDL2/SDL_surface.h hdr/../SDL2/SDL_blendmode.h \
+  hdr/../SDL2/SDL_keyboard.h hdr/../SDL2/SDL_keycode.h \
+  hdr/../SDL2/SDL_scancode.h hdr/../SDL2/SDL_mouse.h \
+  hdr/../SDL2/SDL_joystick.h hdr/../SDL2/SDL_gamecontroller.h \
+  hdr/../SDL2/SDL_sensor.h hdr/../SDL2/SDL_quit.h \
+  hdr/../SDL2/SDL_gesture.h hdr/../SDL2/SDL_touch.h \
+  hdr/../SDL2/SDL_filesystem.h hdr/../SDL2/SDL_haptic.h \
+  hdr/../SDL2/SDL_hidapi.h hdr/../SDL2/SDL_hints.h \
+  hdr/../SDL2/SDL_loadso.h hdr/../SDL2/SDL_log.h \
+  hdr/../SDL2/SDL_messagebox.h hdr/../SDL2/SDL_metal.h \
+  hdr/../SDL2/SDL_power.h hdr/../SDL2/SDL_render.h \
+  hdr/../SDL2/SDL_shape.h hdr/../SDL2/SDL_system.h \
+  hdr/../SDL2/SDL_timer.h hdr/../SDL2/SDL_version.h \
+  hdr/../SDL2/SDL_locale.h hdr/../SDL2/SDL_misc.h \
+  hdr/../SDL2/SDL_image.h hdr/../SDL2/SDL_ttf.h hdr/../SDL2/SDL_mixer.h \
+  hdr/tools/vector.h hdr/tools/color.h hdr/render/sprite.h \
+  hdr/render/../ab.h hdr/statStructs.h hdr/battleClasses/character.h \
+  hdr/battleClasses/../ab.h hdr/particle/dust.h hdr/particle/../ab.h \
+  hdr/battleClasses/turnOrder.h hdr/battleClasses/turnIndicator.h \
+  hdr/battleClasses/killEffect.h hdr/battleClasses/kill.h \
+  hdr/particle/particle.h hdr/particle/particleManager.h \
+  hdr/info/snippet.h hdr/info/../ab.h hdr/info/flyingText.h \
+  hdr/tools/throwArch.h hdr/tools/../ab.h hdr/tools/spriteMover.h \
+  hdr/info/texts/textBubble.h hdr/info/texts/../../ab.h \
+  hdr/UI/texts/UiTextUpdater.h hdr/UI/texts/../../ab.h hdr/UI/number.h \
+  hdr/UI/../ab.h hdr/UI/marker.h hdr/UI/statuses.h hdr/UI/bar.h \
+  hdr/UI/fatigueBar.h hdr/UI/button.h hdr/UI/energy.h \
+  hdr/objects/toolBox.h hdr/objects/../ab.h hdr/objects/object.h \
+  hdr/objects/objUpdate.h hdr/effects/buffEffect.h hdr/effects/../ab.h \
+  hdr/effects/hostEffect.h hdr/effects/healEffect.h \
+  hdr/effects/frenzyColorer.h hdr/effects/thugInspire.h \
+  hdr/effects/slowedEffect.h hdr/effects/nailBombBlast.h \
+  hdr/effects/hostEyesEffect.h hdr/effects/acidBombBlast.h \
+  hdr/effects/shieldBashEffect.h hdr/info/healthColoring.h \
+  hdr/info/supplyEffect.h hdr/info/control.h hdr/info/colorEffect.h \
+  hdr/info/controlSetter.h hdr/info/counter.h hdr/info/stunUpdates.h \
+  hdr/info/movementEnergy.h hdr/info/infoBar.h \
+  hdr/info/IconBars/IconHealth.h hdr/info/IconBars/../../ab.h \
+  hdr/info/IconBars/IconBar.h hdr/info/IconBars/IconBarHandler.h \
+  hdr/info/menu/menu.h hdr/info/menu/../../ab.h \
+  hdr/info/texts/hitChance.h hdr/info/texts/statusInfos.h \
+  hdr/info/info.h hdr/battleClasses/groundColoring.h \
+  hdr/ability/damages/addStatus.h hdr/ability/damages/../../ab.h \
+  hdr/ability/characterMover.h hdr/ability/../ab.h \
+  hdr/ability/objects/fireBall.h hdr/ability/objects/../../ab.h \
+  hdr/ability/objects/phantomKnight.h hdr/ability/objects/teleport.h \
+  hdr/ability/objects/lightningBolt.h hdr/ability/objects/rockFall.h \
+  hdr/ability/objects/healthTransfer.h hdr/ability/animation/rotate.h \
+  hdr/ability/animation/../../ab.h hdr/ability/animation/goForTheHead.h \
+  hdr/ability/animation/supply.h hdr/ability/animation/shieldBash.h \
+  hdr/ability/animation/knightAttack.h \
+  hdr/ability/animation/hostEyesAnim.h \
+  hdr/ability/animation/pickUpToolBox.h \
+  hdr/ability/animation/supplyAlly.h hdr/ability/animation/axeSlash.h \
+  hdr/ability/animation/bigThugInspire.h \
+  hdr/ability/animation/thugStrike.h \
+  hdr/ability/animation/bigThugStrike.h hdr/ability/animation/nailBomb.h \
+  hdr/ability/animation/acidBombAnim.h \
+  hdr/ability/animation/axeJumpAnim.h \
+  hdr/ability/animation/thoolThrowAnim.h \
+  hdr/ability/animation/toxinBuff.h hdr/ability/animation/smithBuff.h \
+  hdr/ability/animation/hammerSmack.h hdr/ability/animation/slowBomb.h \
+  hdr/ability/animation/skeleMelee.h \
+  hdr/ability/animation/incinerateAnim.h \
+  hdr/ability/animation/flameBlast.h hdr/ability/animation/phantomAnim.h \
+  hdr/ability/animation/flamePortAnim.h \
+  hdr/ability/animation/daggerSlashAnim.h \
+  hdr/ability/animation/witchAnimation.h \
+  hdr/ability/animation/mageAttack.h hdr/ability/animation/controlZone.h \
+  hdr/ability/animation/smokeBombAnim.h \
+  hdr/ability/animation/heartBeat.h hdr/ability/animation/teleStart.h \
+  hdr/ability/animation/lionSmackAnim.h \
+  hdr/ability/animation/daggerThrowAnim.h \
+  hdr/ability/animation/raiderBlock.h hdr/ability/animation/skeleLunge.h \
+  hdr/ability/animation/acidRain.h hdr/ability/animation/flameSlash.h \
+  hdr/ability/objects/smokeEffect.h hdr/ability/objects/smokeBomb.h \
+  hdr/ability/objects/dagger.h hdr/ability/objects/bashMover.h \
+  hdr/ability/damages/damageCreator.h hdr/ability/effectUpdater.h \
+  hdr/ability/damages/Damager.h hdr/ability/damages/opportunity.h \
+  hdr/ability/damages/abilityOpportunity.h \
+  hdr/ability/selectors/allSelector.h hdr/ability/selectors/../../ab.h \
+  hdr/ability/selectors/selector.h \
+  hdr/ability/selectors/selectorForTiles.h \
+  hdr/ability/selectors/multiSelector.h \
+  hdr/ability/selectors/phantomSelector.h \
+  hdr/ability/selectors/selectorQueue.h \
+  hdr/ability/selectors/axeJumpSelector.h \
+  hdr/ability/animation/genericToolThrow.h hdr/ability/abilities.h \
+  hdr/UI/battleUI.h hdr/AI/AiData.h hdr/AI/../ab.h hdr/AI/AiIterator.h \
+  hdr/AI/AiIterator2.h hdr/AI/AiObjectHolder.h hdr/render/renderer.h \
+  hdr/battleClasses/battleGround.h
+
+srcs/ability/damages/../../../hdr/global.h:
+
+hdr/ab.h:
+
+hdr/../SDL2/SDL.h:
+
+hdr/../SDL2/SDL_main.h:
+
+hdr/../SDL2/SDL_stdinc.h:
+
+hdr/../SDL2/SDL_config.h:
+
+hdr/../SDL2/SDL_platform.h:
+
+hdr/../SDL2/begin_code.h:
+
+hdr/../SDL2/close_code.h:
+
+hdr/../SDL2/SDL_assert.h:
+
+hdr/../SDL2/SDL_atomic.h:
+
+hdr/../SDL2/SDL_audio.h:
+
+hdr/../SDL2/SDL_error.h:
+
+hdr/../SDL2/SDL_endian.h:
+
+hdr/../SDL2/SDL_mutex.h:
+
+hdr/../SDL2/SDL_thread.h:
+
+hdr/../SDL2/SDL_rwops.h:
+
+hdr/../SDL2/SDL_clipboard.h:
+
+hdr/../SDL2/SDL_cpuinfo.h:
+
+hdr/../SDL2/SDL_events.h:
+
+hdr/../SDL2/SDL_video.h:
+
+hdr/../SDL2/SDL_pixels.h:
+
+hdr/../SDL2/SDL_rect.h:
+
+hdr/../SDL2/SDL_surface.h:
+
+hdr/../SDL2/SDL_blendmode.h:
+
+hdr/../SDL2/SDL_keyboard.h:
+
+hdr/../SDL2/SDL_keycode.h:
+
+hdr/../SDL2/SDL_scancode.h:
+
+hdr/../SDL2/SDL_mouse.h:
+
+hdr/../SDL2/SDL_joystick.h:
+
+hdr/../SDL2/SDL_gamecontroller.h:
+
+hdr/../SDL2/SDL_sensor.h:
+
+hdr/../SDL2/SDL_quit.h:
+
+hdr/../SDL2/SDL_gesture.h:
+
+hdr/../SDL2/SDL_touch.h:
+
+hdr/../SDL2/SDL_filesystem.h:
+
+hdr/../SDL2/SDL_haptic.h:
+
+hdr/../SDL2/SDL_hidapi.h:
+
+hdr/../SDL2/SDL_hints.h:
+
+hdr/../SDL2/SDL_loadso.h:
+
+hdr/../SDL2/SDL_log.h:
+
+hdr/../SDL2/SDL_messagebox.h:
+
+hdr/../SDL2/SDL_metal.h:
+
+hdr/../SDL2/SDL_power.h:
+
+hdr/../SDL2/SDL_render.h:
+
+hdr/../SDL2/SDL_shape.h:
+
+hdr/../SDL2/SDL_system.h:
+
+hdr/../SDL2/SDL_timer.h:
+
+hdr/../SDL2/SDL_version.h:
+
+hdr/../SDL2/SDL_locale.h:
+
+hdr/../SDL2/SDL_misc.h:
+
+hdr/../SDL2/SDL_image.h:
+
+hdr/../SDL2/SDL_ttf.h:
+
+hdr/../SDL2/SDL_mixer.h:
+
+hdr/tools/vector.h:
+
+hdr/tools/color.h:
+
+hdr/render/sprite.h:
+
+hdr/render/../ab.h:
+
+hdr/statStructs.h:
+
+hdr/battleClasses/character.h:
+
+hdr/battleClasses/../ab.h:
+
+hdr/particle/dust.h:
+
+hdr/particle/../ab.h:
+
+hdr/battleClasses/turnOrder.h:
+
+hdr/battleClasses/turnIndicator.h:
+
+hdr/battleClasses/killEffect.h:
+
+hdr/battleClasses/kill.h:
+
+hdr/particle/particle.h:
+
+hdr/particle/particleManager.h:
+
+hdr/info/snippet.h:
+
+hdr/info/../ab.h:
+
+hdr/info/flyingText.h:
+
+hdr/tools/throwArch.h:
+
+hdr/tools/../ab.h:
+
+hdr/tools/spriteMover.h:
+
+hdr/info/texts/textBubble.h:
+
+hdr/info/texts/../../ab.h:
+
+hdr/UI/texts/UiTextUpdater.h:
+
+hdr/UI/texts/../../ab.h:
+
+hdr/UI/number.h:
+
+hdr/UI/../ab.h:
+
+hdr/UI/marker.h:
+
+hdr/UI/statuses.h:
+
+hdr/UI/bar.h:
+
+hdr/UI/fatigueBar.h:
+
+hdr/UI/button.h:
+
+hdr/UI/energy.h:
+
+hdr/objects/toolBox.h:
+
+hdr/objects/../ab.h:
+
+hdr/objects/object.h:
+
+hdr/objects/objUpdate.h:
+
+hdr/effects/buffEffect.h:
+
+hdr/effects/../ab.h:
+
+hdr/effects/hostEffect.h:
+
+hdr/effects/healEffect.h:
+
+hdr/effects/frenzyColorer.h:
+
+hdr/effects/thugInspire.h:
+
+hdr/effects/slowedEffect.h:
+
+hdr/effects/nailBombBlast.h:
+
+hdr/effects/hostEyesEffect.h:
+
+hdr/effects/acidBombBlast.h:
+
+hdr/effects/shieldBashEffect.h:
+
+hdr/info/healthColoring.h:
+
+hdr/info/supplyEffect.h:
+
+hdr/info/control.h:
+
+hdr/info/colorEffect.h:
+
+hdr/info/controlSetter.h:
+
+hdr/info/counter.h:
+
+hdr/info/stunUpdates.h:
+
+hdr/info/movementEnergy.h:
+
+hdr/info/infoBar.h:
+
+hdr/info/IconBars/IconHealth.h:
+
+hdr/info/IconBars/../../ab.h:
+
+hdr/info/IconBars/IconBar.h:
+
+hdr/info/IconBars/IconBarHandler.h:
+
+hdr/info/menu/menu.h:
+
+hdr/info/menu/../../ab.h:
+
+hdr/info/texts/hitChance.h:
+
+hdr/info/texts/statusInfos.h:
+
+hdr/info/info.h:
+
+hdr/battleClasses/groundColoring.h:
+
+hdr/ability/damages/addStatus.h:
+
+hdr/ability/damages/../../ab.h:
+
+hdr/ability/characterMover.h:
+
+hdr/ability/../ab.h:
+
+hdr/ability/objects/fireBall.h:
+
+hdr/ability/objects/../../ab.h:
+
+hdr/ability/objects/phantomKnight.h:
+
+hdr/ability/objects/teleport.h:
+
+hdr/ability/objects/lightningBolt.h:
+
+hdr/ability/objects/rockFall.h:
+
+hdr/ability/objects/healthTransfer.h:
+
+hdr/ability/animation/rotate.h:
+
+hdr/ability/animation/../../ab.h:
+
+hdr/ability/animation/goForTheHead.h:
+
+hdr/ability/animation/supply.h:
+
+hdr/ability/animation/shieldBash.h:
+
+hdr/ability/animation/knightAttack.h:
+
+hdr/ability/animation/hostEyesAnim.h:
+
+hdr/ability/animation/pickUpToolBox.h:
+
+hdr/ability/animation/supplyAlly.h:
+
+hdr/ability/animation/axeSlash.h:
+
+hdr/ability/animation/bigThugInspire.h:
+
+hdr/ability/animation/thugStrike.h:
+
+hdr/ability/animation/bigThugStrike.h:
+
+hdr/ability/animation/nailBomb.h:
+
+hdr/ability/animation/acidBombAnim.h:
+
+hdr/ability/animation/axeJumpAnim.h:
+
+hdr/ability/animation/thoolThrowAnim.h:
+
+hdr/ability/animation/toxinBuff.h:
+
+hdr/ability/animation/smithBuff.h:
+
+hdr/ability/animation/hammerSmack.h:
+
+hdr/ability/animation/slowBomb.h:
+
+hdr/ability/animation/skeleMelee.h:
+
+hdr/ability/animation/incinerateAnim.h:
+
+hdr/ability/animation/flameBlast.h:
+
+hdr/ability/animation/phantomAnim.h:
+
+hdr/ability/animation/flamePortAnim.h:
+
+hdr/ability/animation/daggerSlashAnim.h:
+
+hdr/ability/animation/witchAnimation.h:
+
+hdr/ability/animation/mageAttack.h:
+
+hdr/ability/animation/controlZone.h:
+
+hdr/ability/animation/smokeBombAnim.h:
+
+hdr/ability/animation/heartBeat.h:
+
+hdr/ability/animation/teleStart.h:
+
+hdr/ability/animation/lionSmackAnim.h:
+
+hdr/ability/animation/daggerThrowAnim.h:
+
+hdr/ability/animation/raiderBlock.h:
+
+hdr/ability/animation/skeleLunge.h:
+
+hdr/ability/animation/acidRain.h:
+
+hdr/ability/animation/flameSlash.h:
+
+hdr/ability/objects/smokeEffect.h:
+
+hdr/ability/objects/smokeBomb.h:
+
+hdr/ability/objects/dagger.h:
+
+hdr/ability/objects/bashMover.h:
+
+hdr/ability/damages/damageCreator.h:
+
+hdr/ability/effectUpdater.h:
+
+hdr/ability/damages/Damager.h:
+
+hdr/ability/damages/opportunity.h:
+
+hdr/ability/damages/abilityOpportunity.h:
+
+hdr/ability/selectors/allSelector.h:
+
+hdr/ability/selectors/../../ab.h:
+
+hdr/ability/selectors/selector.h:
+
+hdr/ability/selectors/selectorForTiles.h:
+
+hdr/ability/selectors/multiSelector.h:
+
+hdr/ability/selectors/phantomSelector.h:
+
+hdr/ability/selectors/selectorQueue.h:
+
+hdr/ability/selectors/axeJumpSelector.h:
+
+hdr/ability/animation/genericToolThrow.h:
+
+hdr/ability/abilities.h:
+
+hdr/UI/battleUI.h:
+
+hdr/AI/AiData.h:
+
+hdr/AI/../ab.h:
+
+hdr/AI/AiIterator.h:
+
+hdr/AI/AiIterator2.h:
+
+hdr/AI/AiObjectHolder.h:
+
+hdr/render/renderer.h:
+
+hdr/battleClasses/battleGround.h:
